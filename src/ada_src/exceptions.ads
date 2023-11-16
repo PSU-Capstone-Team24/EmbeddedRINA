9d749976cad4199f07c44150a11748f66690c0e8
Mason Ticehurst <masonticehurst@me.com>
2023-11-15 20:12:27 -0500

    Add Flow_Allocation failure exception

--- src/ada_src/exceptions.ads
+++ src/ada_src/exceptions.ads
@@ -2,4 +2,5 @@ package Exceptions is
    Unexpected_Flow_Failure : exception;
    DIF_Registration_Failure : exception;
    RINA_Control_Failure : exception;
+   Flow_Alloc_Failure : exception;
 end Exceptions;
