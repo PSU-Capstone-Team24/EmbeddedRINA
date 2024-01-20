1f96a3eaaf2cf862b8a27df93c0f9b0c915bd6dd
Mason Ticehurst <masonticehurst@me.com>
2024-01-20 12:59:45 -0500

    Update light-tasking runtime to embedded runtime, Add IPCP hashmap

--- eRINA_STM32F7/src/rina/demo.adb
+++ eRINA_STM32F7/src/rina/demo.adb
@@ -1,6 +1,14 @@
 with Demos;
+with Net;
+with Ada.Strings.Hash;
+with Ada.Strings.Bounded;
+with Ada.Containers.Indefinite_Hashed_Maps;
 
 procedure Demo is
+
+   --  Mac address type
+   use Net;
+
    procedure Header is
    begin
       Demos.Put (0, 0, "eRINA_Debug");
@@ -10,12 +18,30 @@ procedure Demo is
    MAX_IPC_COUNT : constant Natural := 32;
    MAX_IPCP_NAME_LENGTH : constant Natural := 128;
 
-   type IPC_Handler_Array is array (1 .. MAX_IPC_COUNT) of String (1 .. MAX_IPCP_NAME_LENGTH);
+   package IPCP_String is new Ada.Strings.Bounded.Generic_Bounded_Length (MAX_IPCP_NAME_LENGTH);
+   use IPCP_String;
+
+   function Hash(Name : in Bounded_String) return Ada.Containers.Hash_Type is begin
+       return Ada.Strings.Hash(To_String (Name));
+   end Hash;
 
    --  Eventually make this the protected object that holds the IPC database
-   IPC_Handlers    : IPC_Handler_Array;
+   package IPCP_MAC_Map is new Ada.Containers.Indefinite_Hashed_Maps (
+       Key_Type => Bounded_String,
+       Element_Type => Ether_Addr,
+       Hash => Hash,
+       Equivalent_Keys => "="
+   );
+   
+   use IPCP_MAC_Map;
+
+   --  Instantiate IPCP map
+   IPCP_Map : Map;
+  
+   --  Debug only, remove later
+   Demo_IPCP_Name : constant Bounded_String := To_Bounded_String ("Demo.IPCP");
+   Demo_IPCP_MacAddr : constant Ether_Addr := (others => 16#ff#);
 begin
-
    Header;
-
+   IPCP_Map.Include (Demo_IPCP_Name, Demo_IPCP_MacAddr);
 end Demo;
