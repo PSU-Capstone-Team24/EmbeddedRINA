668882bc206fd5cf9cc9fbb2fa6364893159b42a
Anthony <immekus21@gmail.com>
2024-01-21 16:45:09 -0500

    Updated TC 005, 008, and 009. Added exception handling to all TC

--- eRINA_Tests/src/test_rina_register.adb
+++ eRINA_Tests/src/test_rina_register.adb
@@ -2,6 +2,7 @@
 pragma Style_Checks (Off);
 
 with AUnit.Assertions;
+with exceptions;
 with AUnit.Test_Caller;
 with GNAT.OS_Lib;
 with Bindings.Rlite.API;
@@ -34,16 +35,16 @@ package body Test_RINA_Register is
          (Name_003 & " Fails when DIF name too long", Test_Register_DIF_Length'Access));
       Test_Suite.Add_Test (Caller.Create
          (Name_004 & " Fails when DIF name is empty", Test_Register_DIF_Empty'Access));
-      --Test_Suite.Add_Test (Caller.Create
-         --(Name_005 & " Fails when DIF name does not exist in IPCP", Test_Register_Not_Exist_IPCP'Access));
+      Test_Suite.Add_Test (Caller.Create
+         (Name_005 & " Fails when DIF name does not exist in IPCP", Test_Register_Not_Exist_IPCP'Access));
       Test_Suite.Add_Test (Caller.Create
          (Name_006 & " Fails when Application name too long", Test_Register_Application_Length'Access));
       Test_Suite.Add_Test (Caller.Create
          (Name_007 & " Fails when Application name is empty", Test_Register_Application_Empty'Access));
-      --Test_Suite.Add_Test (Caller.Create
-         --(Name_008 & " Fails when Application name has already been registered", Test_Register_Not_Exist_IPCP'Access));
-      --Test_Suite.Add_Test (Caller.Create
-         --(Name_009 & " Verify rina_register successfully reg. an App name to DIF", Test_Register_AppName_to_DIF'Access));
+      Test_Suite.Add_Test (Caller.Create
+         (Name_008 & " Fails when Application name has already been registered", Test_Register_Not_Exist_IPCP'Access));
+      Test_Suite.Add_Test (Caller.Create
+         (Name_009 & " Verify rina_register successfully reg. an App name to DIF", Test_Register_AppName_to_DIF'Access));
       Test_Suite.Add_Test (Caller.Create
          (Name_010 & " Verify rina_register returns -1 when inval. file descriptor is passed", Test_Register_Application_Empty'Access));
 
@@ -80,10 +81,13 @@ package body Test_RINA_Register is
       DIF_Name : Bounded_String := To_Bounded_String ("TestDIF"); 
       Register_Success : File_Descriptor := Invalid_FD;
       RINA_Dev_FD : File_Descriptor := RINA_Open;
+      Caused_Error : Boolean := False;
    begin
       Register_Success := RINA_Register (RINA_Dev_FD, DIF_Name, To_Bounded_String ("TestApplicationName"), 0);
-      --if fails, assert prints
-      Assert(Register_Success = Invalid_FD, "IPCP already exist");
+   exception
+      when EXCEPTIONS.DIF_REGISTRATION_FAILURE =>
+         Caused_Error := True;
+      Assert(Caused_Error, "DIF_Name does not exist in IPCP");
    end Test_Register_Not_Exist_IPCP;
 
    --Test Case 006
@@ -117,18 +121,27 @@ package body Test_RINA_Register is
       RINA_Dev_FD : File_Descriptor := RINA_Open;
       Register_First : File_Descriptor := RINA_Register (RINA_Dev_FD, App_Name, To_Bounded_String ("TestDIF"), 0);
       Register_Second : File_Descriptor := RINA_Register (RINA_Dev_FD, App_Name, To_Bounded_String ("TestDIF"), 0);
+      Caused_Error : Boolean := False;
    begin
-   -- The first registration should succeed, the second should fail
-      Assert (Register_First /= Invalid_FD and Register_Second = Invalid_FD, "Duplicate application name registration did not fail");
+      Register_Second := RINA_Register (RINA_Dev_FD, App_Name, To_Bounded_String ("TestDIF"), 0);
+   exception
+      when EXCEPTIONS.DIF_REGISTRATION_FAILURE =>
+         Caused_Error := True;
+      Assert (Caused_Error, "Application name could be registered twice");
    end Test_Register_App_Already_Reg;
 
    -- Test Case 009
    procedure Test_Register_AppName_to_DIF (Object : in out Test) is
-      App_Name : Bounded_String := To_Bounded_String ("TestApplicationName");
-      RINA_Dev_FD : File_Descriptor := RINA_Open;
-      Register_Success : File_Descriptor := RINA_Register (RINA_Dev_FD, App_Name, To_Bounded_String ("TestDIF"), 0);
+   App_Name : Bounded_String := To_Bounded_String ("UniqueTestApp");
+   RINA_Dev_FD : File_Descriptor := RINA_Open;
+   Register_Success : File_Descriptor;
+   Caused_Error : Boolean := False;
    begin
-      Assert (Register_Success /= Invalid_FD, "Failed to register a unique application name");
+      Register_Success := RINA_Register (RINA_Dev_FD, App_Name, To_Bounded_String ("TestDIF"), 0);
+   exception
+      when EXCEPTIONS.DIF_REGISTRATION_FAILURE =>
+         Caused_Error := True;
+      Assert (Caused_Error, "Failed to register a unique application name");
    end Test_Register_AppName_to_DIF;
 
    -- Test Case 010
