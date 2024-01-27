670d9ad9f25f7e6f4f18afb0049e138d372fc75e
Keian Kaserman <kaserman22@gmail.com>
2024-01-27 18:25:09 -0500

    Add Test Suite 3 to test case runner

--- eRINA_Tests/src/erina_tests.adb
+++ eRINA_Tests/src/erina_tests.adb
@@ -21,6 +21,7 @@ procedure eRINA_Tests is
    begin
       Result.Add_Test (Test_RINA_Open.Suite);
       Result.Add_Test (Test_RINA_Register.Suite);
+      Result.Add_Test (Test_RINA_Unregister.Suite);
 
       Result.Add_Test (Test_RINA_Flow_Alloc_Wait.Suite);
       Result.Add_Test (Test_RINA_Flow_Alloc.Suite);
