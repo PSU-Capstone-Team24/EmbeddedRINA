9b5a2f7d14aac6225f57308d2572b4216b011ec1
Mason Ticehurst <masonticehurst@me.com>
2024-01-28 22:37:31 -0500

    Add signed subtypes

--- eRINA_STM32F7/src/net/buffers.ads
+++ eRINA_STM32F7/src/net/buffers.ads
@@ -4,12 +4,18 @@ with Interfaces;
   use Interfaces;
 
 package Buffers is
-   --  subtypes copied from ada_enet
+   --  unsigned subtypes copied from ada_enet
    subtype Uint8 is Unsigned_8;
    subtype Uint16 is Unsigned_16;
    subtype Uint32 is Unsigned_32;
    subtype Uint64 is Unsigned_64;
+   
+   --  signed subtypes
+   subtype Int8 is Short_Short_Integer;
+   subtype Int16 is Short_Integer;
+   subtype Int32 is Integer;
+   subtype Int64 is Long_Long_Integer;
 
-    type Byte is new Uint8;
-    type Byte_Buffer is array (Natural range <>) of Byte;
+   type Byte is new Uint8;
+   type Byte_Buffer is array (Natural range <>) of Byte;
 end Buffers;
