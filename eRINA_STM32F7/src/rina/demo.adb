abe1995a5d5bc9baf4a559c148a9e64d431afc16
Mason Ticehurst <masonticehurst@me.com>
2024-02-06 19:56:47 -0500

    Add loop to demo to prevent immediate exit, Add Initialize_Blank to setup LCD without enet and screen text

--- eRINA_STM32F7/src/rina/demo.adb
+++ eRINA_STM32F7/src/rina/demo.adb
@@ -11,37 +11,15 @@ procedure Demo is
 
    procedure Header is
    begin
-      Demos.Put (0, 0, "eRINA_Debug");
+      Demos.Put (0, 0, "eRINA Debug");
    end Header;
 
-   --  Max of 32 IPCs can be registered
-   MAX_IPC_COUNT : constant Natural := 32;
-   MAX_IPCP_NAME_LENGTH : constant Natural := 128;
-
-   package IPCP_String is new Ada.Strings.Bounded.Generic_Bounded_Length (MAX_IPCP_NAME_LENGTH);
-   use IPCP_String;
-
-   function Hash(Name : in Bounded_String) return Ada.Containers.Hash_Type is begin
-       return Ada.Strings.Hash(To_String (Name));
-   end Hash;
-
-   --  Eventually make this the protected object that holds the IPC database
-   package IPCP_MAC_Map is new Ada.Containers.Indefinite_Hashed_Maps (
-       Key_Type => Bounded_String,
-       Element_Type => Ether_Addr,
-       Hash => Hash,
-       Equivalent_Keys => "="
-   );
-   
-   use IPCP_MAC_Map;
-
-   --  Instantiate IPCP map
-   IPCP_Map : Map;
-  
-   --  Debug only, remove later
-   Demo_IPCP_Name : constant Bounded_String := To_Bounded_String ("Demo.IPCP");
-   Demo_IPCP_MacAddr : constant Ether_Addr := (others => 16#ff#);
+   procedure Initialize is new Demos.Initialize_Blank (Header);
 begin
-   Header;
-   IPCP_Map.Include (Demo_IPCP_Name, Demo_IPCP_MacAddr);
+   Initialize;
+
+   --  Keep board from immediately terminating
+   loop
+      null;
+   end loop;
 end Demo;
