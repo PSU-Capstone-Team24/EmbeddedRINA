9a8f55cb497499ddce7b32b8d6ab2c7f70fe898a
Mason Ticehurst <masonticehurst@me.com>
2024-02-09 15:11:52 -0500

    Add debug package, move console printing to Debug

---
+++ eRINA_STM32F7/src/rina/debug.ads
@@ -0,0 +1,19 @@
+with HAL.Bitmap;
+with BMP_Fonts;
+with GUI;
+
+package Debug is
+   type Debug is (Error, Warning, Info);
+
+   CONSOLE_STARTING_POINT   : HAL.Bitmap.Point := (0, 100);
+   CURRENT_CONSOLE_POSITION : HAL.Bitmap.Point := (0, 100);
+   FONT_HEIGHT              : constant Natural :=
+     BMP_Fonts.Char_Height (Font => BMP_Fonts.Font8x8);
+   MAX_LINES    : constant Natural := 20;
+   LINE_PADDING : constant Natural := 2;
+
+   procedure Print (Debug_Level : in Debug; Msg : in String);
+   procedure CopyLine
+     (SrcLineNumber : in Natural; DstLineNumer : in Natural;
+      DeleteSrc     : in Boolean);
+end Debug;
