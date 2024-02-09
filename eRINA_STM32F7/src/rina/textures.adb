39d7879cbf24b470bbcbdb8a888906b0aa016e08
Mason Ticehurst <masonticehurst@me.com>
2024-02-09 15:12:19 -0500

    Add Texture package, Add Texture.Print procedure, Add PSU logo texture

---
+++ eRINA_STM32F7/src/rina/textures.adb
@@ -0,0 +1,19 @@
+with Bitmap_Color_Conversion;
+with STM32.Board;
+
+package body Textures is
+   procedure Print (T : in Texture; Pos : in HAL.Bitmap.Point) is
+   begin
+      for Row in 0 .. Textures.Texture_Height - 1 loop
+         for Column in 0 .. Textures.Texture_Width - 1 loop
+            STM32.Board.Display.Hidden_Buffer (1).Set_Source
+              (Bitmap_Color_Conversion.Word_To_Bitmap_Color
+                 (HAL.Bitmap.ARGB_1555, HAL.UInt32 (T (Row) (Column))));
+            STM32.Board.Display.Hidden_Buffer (1).Set_Pixel
+              ((Pos.X + Row, Pos.Y + Column));
+         end loop;
+      end loop;
+
+      STM32.Board.Display.Update_Layer (1);
+   end Print;
+end Textures;
