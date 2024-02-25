6a046eaa87e5b9e0c5ec6ca1e296ea4e268afd36
Mason Ticehurst <masonticehurst@me.com>
2024-02-24 23:41:53 -0500

    Add Add_Button, Draw_Rounded_Rectangle,  Fill_Rounded_Rectangle, Print_Large. Modify UI to match presentation demo

--- eRINA_STM32F7/src/rina/gui.adb
+++ eRINA_STM32F7/src/rina/gui.adb
@@ -31,6 +31,26 @@ package body GUI is
       Fill_Rect (Buffer => Screen_Buffer.all, Area => (P, S.Width, S.Height));
    end Draw_Rectangle;
 
+   procedure Draw_Rounded_Rectangle (P : Point; S : Size; C : Bitmap_Color; R : Natural; T : Natural) is
+   begin
+      Set_Source (Buffer => Screen_Buffer.all, ARGB => C);
+      Draw_Rounded_Rect (Buffer => Screen_Buffer.all, Area => (P, S.Width, S.Height), Radius => R, Thickness => T);
+   end Draw_Rounded_Rectangle;
+
+   procedure Fill_Rounded_Rectangle (P : Point; S : Size; C : Bitmap_Color; R : Natural) is
+   begin
+      Set_Source (Buffer => Screen_Buffer.all, ARGB => C);
+      Fill_Rounded_Rect (Buffer => Screen_Buffer.all, Area => (P, S.Width, S.Height), Radius => R);
+   end Fill_Rounded_Rectangle;
+
+   procedure Add_Button
+     (P : Point; S : Size; C : Bitmap_Color; T : Access_String)
+   is
+      Btn : Button := (P, S, C, T);
+   begin
+      Buttons.Append (Btn);
+   end Add_Button;
+
    procedure Print (Msg : in String; Pos : in Point) is
    begin
       Bitmapped_Drawing.Draw_String
@@ -38,6 +58,14 @@ package body GUI is
          Msg => Msg, Font => BMP_Fonts.Font8x8, Foreground => Foreground,
          Background => Background);
    end Print;
+
+   procedure Print_Large (Msg : in String; Pos : in Point) is
+   begin
+      Bitmapped_Drawing.Draw_String
+        (Buffer     => Screen_Buffer.all, Start => Scale ((Pos.X, Pos.Y)),
+         Msg => Msg, Font => BMP_Fonts.Font12x12, Foreground => Foreground,
+         Background => Background);
+   end Print_Large;
 
    procedure Initialize is
    begin
--- eRINA_STM32F7/src/rina/gui.ads
+++ eRINA_STM32F7/src/rina/gui.ads
@@ -1,32 +1,49 @@
 with BMP_Fonts;
 with HAL.Bitmap; use HAL.Bitmap;
+with Ada.Containers.Vectors;
 
 package GUI is
    pragma Warnings (Off);
 
    Large_Font : BMP_Fonts.BMP_Font := BMP_Fonts.Font12x12;
-   Foreground : Bitmap_Color       := White;
-   Background : Bitmap_Color       := Black;
-
+   Foreground : Bitmap_Color       := Black;
+   Background : Bitmap_Color       := White;
+   Button_Color : Bitmap_Color     := (255, 242, 243, 245);
+   Build_Verson : String           := "0.0.1";
+   
    type Size is record
       Width  : Natural;
       Height : Natural;
    end record;
 
+   type Access_String is access all String;
+
    type Button is record
       Position : Point;
       BSize    : Size;
+      Color    : Bitmap_Color;
+      Text     : Access_String;
    end record;
 
    Board_Resolution : Size    := (480, 272);
-   Frame_Rate       : Natural := 30;
+   Frame_Rate       : Natural := 60;
+   Max_Buttons      : Natural := 16;
+
+   package Button_Vectors is new Ada.Containers.Vectors
+     (Index_Type => Natural, Element_Type => Button);
+
+   Buttons : Button_Vectors.Vector;
 
    procedure Initialize;
    procedure Update;
    procedure Print (Msg : in String; Pos : in Point);
+   procedure Print_Large (Msg : in String; Pos : in Point);
    function Screen_Buffer return Any_Bitmap_Buffer;
    procedure Draw_Rectangle (P : Point; S : Size; C : Bitmap_Color);
-
+   procedure Draw_Rounded_Rectangle (P : Point; S : Size; C : Bitmap_Color; R : Natural; T : Natural);
+   procedure Fill_Rounded_Rectangle (P : Point; S : Size; C : Bitmap_Color; R : Natural);
+   procedure Add_Button
+     (P : Point; S : Size; C : Bitmap_Color; T : Access_String);
    function MeasureText
      (Text : in String; Font : in BMP_Fonts.BMP_Font) return Size;
    function Scale (Position : in Point) return Point;
