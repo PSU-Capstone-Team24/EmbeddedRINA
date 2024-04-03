443ab6f3b7f273157a3219ae6109b16c6dd5e39f
Mason Ticehurst <masonticehurst@me.com>
2024-04-03 19:52:13 -0400

    Fix OS.Read blocking test suite runner

--- eRINA_Linux/src/messages/bindings-rlite-msg.adb
+++ eRINA_Linux/src/messages/bindings-rlite-msg.adb
@@ -1,72 +1,58 @@
 --  Temp disabling
 pragma Style_Checks (Off);
 
+with GNAT.OS_Lib; use GNAT.OS_Lib;
 with Bindings.Rlite.Api; use Bindings.Rlite.Api;
 with Exceptions;
 with Debug;
 
 package body Bindings.Rlite.Msg is
-   
+
+   EWOULDBLOCK : constant Integer := 11;
+
+   function C_fcntl (fd : Uint32; cmd : Uint32; value : Uint32) return Uint32;
+   pragma Import (C, C_fcntl, "fcntl");
+
    function Read_Next_Msg
      (Rfd : OS.File_Descriptor) return Byte_Buffer
    is
-      Timeout_Exception : exception;
-
-   function OS_Read return Byte_Buffer is
       Bytes_Read : Integer;
       Buffer : Byte_Buffer(1 .. 4096) := (others => 0);
+      Flags : Uint32 := C_fcntl (Uint32 (Rfd), 16#0003#, 0);
    begin
-
-      -- Perform the blocking read operation
-      Bytes_Read := OS.Read (Rfd, Buffer'Address, Buffer'Length);
-
-      --  Make sure we've actually read something
-      if Bytes_Read < 0 then
-         Debug.Print
-         ("Read_Next_Msg", "Error reading from file descriptor",
-            Debug.Error);
+      if Rfd = Invalid_FD then
          raise Exceptions.RINA_Control_Failure;
       end if;
+      
+      Flags := C_fcntl (Uint32 (Rfd), 16#0004#, Flags or 8#004000#);
+
+      for I in 1 .. 4 loop
+         -- Perform the blocking read operation
+         Bytes_Read := OS.Read (Rfd, Buffer'Address, Buffer'Length);
+
+         --  Make sure we've actually read something
+         if Bytes_Read < 0 then
+            if Errno /= EWOULDBLOCK then
+               Debug.Print
+               ("Read_Next_Msg", "Error reading from file descriptor",
+                  Debug.Error);
+
+               raise Exceptions.RINA_Control_Failure;
+            end if;
+         else
+            exit;
+         end if;
+
+         --  Has failed to read bytes for 3 iterations
+         if I = 4 then
+            raise Exceptions.RINA_Control_Failure;
+         end if;
+
+         -- Delay a bit before trying again
+         delay 0.25;
+      end loop;
 
       return Buffer;
-   end OS_Read;
-
-   task OS_Read_Task is
-      entry Start;
-      entry Get (Result : out Byte_Buffer);
-   end OS_Read_Task;
-
-   task body OS_Read_Task is
-      Buffer : Byte_Buffer(1 .. 4096);
-   begin
-      accept Start;
-
-      Buffer := OS_Read;
-      
-      select
-         accept Get (Result : out Byte_Buffer) do
-            Result := Buffer;
-         end Get;
-      or
-         terminate;
-      end select;
-   end OS_Read_Task;
-
-      Final_Result : Byte_Buffer(1 .. 4096) := (others => 0);
-   begin
-      OS_Read_Task.Start;
-
-      select
-         OS_Read_Task.Get (Final_Result);
-         return Final_Result;
-      or
-         delay 0.5;
-
-         --  MT: TODO: For some reason this still blocks, preventing execution
-         --  This needs to be investigated further.
-         Debug.Print("Read_Next_Msg", "OS.Read Timeout!", Debug.Error);
-         raise Timeout_Exception;
-      end select;
    end Read_Next_Msg;
 
 end Bindings.Rlite.Msg;
