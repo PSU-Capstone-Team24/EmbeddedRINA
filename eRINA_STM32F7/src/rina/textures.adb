58fbb6e55365d3c44d1dc75f99133e0c928b6ec6
Mason Ticehurst <masonticehurst@me.com>
2024-04-11 23:55:29 -0400

    Don't draw pixel in texture if it is fully transparent

--- eRINA_STM32F7/src/rina/textures.adb
+++ eRINA_STM32F7/src/rina/textures.adb
@@ -9,7 +9,11 @@ package body Textures is
             GUI.Screen_Buffer.Set_Source
               (Bitmap_Color_Conversion.Word_To_Bitmap_Color
                  (HAL.Bitmap.ARGB_1555, HAL.UInt32 (T (Row) (Column))));
-            GUI.Screen_Buffer.Set_Pixel ((Pos.X + Row, Pos.Y + Column));
+            
+            --  If this byte is supposed to be transparent, don't even draw it
+            if T (Row)(Column) > 16#0FFF# then
+               GUI.Screen_Buffer.Set_Pixel ((Pos.X + Row, Pos.Y + Column));
+            end if;
          end loop;
       end loop;
    end Print;
